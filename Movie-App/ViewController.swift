976cc702de3803e1801a6a7981504a8767febe01
Jalaj Deotwal <ronideotwal@gmail.com>
2025-12-09 22:23:44 +0900

    Initial Commit

---
+++ Movie-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-App
+//
+//  Created by Deotwal, Jalaj | Ronnie on 2025/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
