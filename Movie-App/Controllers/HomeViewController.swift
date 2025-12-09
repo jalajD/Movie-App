fc22f588480c3545556f568edebf9bc95751c455
Jalaj Deotwal <ronideotwal@gmail.com>
2025-12-10 02:53:00 +0900

    Setting up home view controller- custom collection view cell and Header view

--- Movie-App/Controllers/HomeViewController.swift
+++ Movie-App/Controllers/HomeViewController.swift
@@ -9,8 +9,81 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let homeFeedTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: "CollectionViewTableViewCell")
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+
+        configureNavBar()
+
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+
+        homeFeedTable.tableHeaderView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+    }
+
+    private func configureNavBar() {
+        let logoImage = UIImage(named: "Logo")?.withRenderingMode(.alwaysOriginal)
+        let logoButton = UIButton(type: .system)
+        logoButton.setImage(logoImage, for: .normal)
+        logoButton.tintColor = .clear
+        logoButton.widthAnchor.constraint(equalToConstant:30).isActive = true
+        logoButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        logoButton.addTarget(self, action: #selector(dummyTapped), for: .touchUpInside)
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoButton)
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+
+        navigationController?.navigationBar.tintColor = .white
+
+    }
+    @objc private func dummyTapped() {
+        // No-op or add future functionality here
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell =  tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
 }
