aaacebf230407dae0afb40304f8f375389d1633d
Jalaj Deotwal <ronideotwal@gmail.com>
2025-12-13 10:17:47 +0900

    Setting up upcoming tab

---
+++ Movie-App/Models/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Movie-App
+//
+//  Created by Deotwal, Jalaj | Ronnie on 2025/12/11.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
